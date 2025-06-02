8665a87076825656b2166e4f0dea8b9a5974f5ba
Groblockia <groblovroum@gmail.com>
2025-06-02 10:13:46 +0200

    door opens and closes automatically

--- Scripts/exit_door.gd
+++ Scripts/exit_door.gd
@@ -1,7 +1,18 @@
 extends StaticBody3D
 
+var is_open = false
+
+signal exit_checkpoint_passed
+
+func _ready() -> void:
+	exit_checkpoint_passed.connect(Global._exit_checkpoint_passed)
 
 func _on_checkpoint_collider_body_entered(body: Node3D) -> void:
-	print("zizi")
 	if body is Player:
-		print("caca")
+		exit_checkpoint_passed.emit()
+
+func open():
+	$AnimationPlayer.play("door_opnening")
+
+func close():
+	$AnimationPlayer.play("door_closing")
