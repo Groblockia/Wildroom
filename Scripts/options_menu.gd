e9734f6f6adcdb3258f661c7155cd8fed3b2a992
Groblockia <groblovroum@gmail.com>
2025-06-12 16:46:22 +0200

    hotfix final fr this time

--- Scripts/options_menu.gd
+++ Scripts/options_menu.gd
@@ -8,11 +8,16 @@ extends Control
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	Global.is_in_optionsMenu = true
+	if Global.is_in_mainMenu:
+		mainMenu_button.visible = false
+	else:
+		mainMenu_button.visible = true
 
 func _process(_delta: float) -> void:
 	print(Global.mouse_sensitivity)
 	mouse_sensitivity_selection()
 	msaa_selection()
+
 
 	if Input.is_action_just_pressed("esc"):
 		call_deferred("_on_back_button_pressed")
@@ -37,5 +42,6 @@ func _on_back_button_pressed() -> void:
 
 func _on_main_menu_button_pressed() -> void:
 	Global.is_in_optionsMenu = false
+	Global.is_in_mainMenu = true
 	queue_free()
 	get_tree().change_scene_to_file("res://Menus/main_menu.tscn")
